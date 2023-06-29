af6f400e566ef145e34e46ff8baccceea4f71814
seguygrah <124881804+seguygrah@users.noreply.github.com>
2023-06-29 21:42:59 +0400

    classlists added, photos added

---
+++ src/classlists.js
@@ -0,0 +1,103 @@
+let classlists = [
+    {
+          id : 1,
+         title: "Mathematics",
+         description: "Provides additional support and instruction to help children develop a strong foundation in math concepts, problem-solving, and analytical skills.",
+         location: "Dubai, Barsha 1",
+         price: 500,
+         image: "math.jpeg",
+         availablespace: 5,
+         
+     },
+     {
+         id: 2,
+         title: "Art",
+         description: "Encourages creativity and imagination through painting, drawing, and crafting.",
+         location: "Dubai, Marina Towers",
+         price: 300,
+         image: "Art.jpeg",
+         availablespace: 5,
+         
+     },
+     {
+         id: 3,
+         title: "Music",
+         description: "Introduces children to music theory, instruments, and singing to improve their musical abilities.",
+         location: "Dubai, JBR",
+         price: 300,
+         image: "music.jpeg",
+         availablespace: 5,
+        
+     },
+     {
+         id: 4,
+         title: "Sports Clubs",
+         description: "Teaches kids the importance of team work while maintaining an active lifestyle",
+         location: "Dubai, JBR",
+         price: 300,
+         image: "sport.png",
+         availablespace: 5,
+         
+     },
+     {
+        id: 5,
+        title: "Coding",
+        description: "Teaches kids the basics of coding and computer programming, preparing them for future technology-related careers.",
+        location: "Dubai, Marina",
+        price: 500,
+        image: "coding.jpeg",
+        availablespace: 5,
+        
+    },
+    {
+        id: 6,
+        title: "Language Classes",
+        description: "Learn a new language such as Spanish, French, or Mandarin through interactive lessons and activities.",
+        location: "Dubai, JBR",
+        price: 350,
+        image: "language.jpeg",
+        availablespace: 5,
+        
+    },
+    {
+        id: 7,
+        title: "Drama",
+        description: "Build confidence and acting skills through improvisation, scene work, and theatre games.",
+        location: "Dubai, Marina",
+        price: 350,
+        image: "drama.jpg",
+        availablespace: 5,
+        
+    },
+    {
+        id: 8,
+        title: "Cooking lessons",
+        description: "Explore the culinary world by learning how to prepare and cook delicious and healthy meals.",
+        location: "Dubai, Barsha 1",
+        price: 300,
+        image: "cooking.jpeg",
+        availablespace: 5,
+     
+    },
+    {
+        id: 9,
+        title: "Science",
+        description: "Conduct experiments and explore scientific concepts through hands-on activities and projects.",
+        location: "Dubai, Barsha 1",
+        price: 400,
+        image: "science.jpeg",
+        availablespace: 5,
+       
+    },
+    {
+        id: 10,
+        title: "Robotics",
+        description: "build and program robots using LEGO kits to explore engineering and technology concepts.",
+        location: "Dubai, JBR",
+        price: 500,
+        image: "robotics.jpeg",
+        availablespace: 5,
+       
+    } 
+     ]
+     
--- src/components/Lesson.vue
+++ src/components/Lesson.vue
@@ -1,47 +1,184 @@
 <template>
-  <div>
-    <div
-      v-for="(lessons, index) in sortedLessons"
-      :key="`lesson--${index}`"
-      class="lessons__item col-md-3 py-2"
-    >
-      <img
-        class="card-img-top"
-        :src="lessons.img"
-        width="200"
-        height="250"
-      />
-      <div class="card-body">
-        <h2 class="card-title">
-          <p>Subject: {{ lessons.subject }}</p>
-        </h2>
-        <p class="card-text">Location: {{ lessons.location }}</p>
-        <p class="card-text">Price: {{ lessons.price }}</p>
-        <div>
-          <p class="card-text">Spaces: {{ lessons.availablespace }}</p>
-          <button
-            class="btn btn-dark"
-            @click="canAddToCart(lessons)"
-            :disabled="!lessons.availablespace"
-          >
-            Add to cart
-          </button>
-          <span v-if="lessons.availablespace">{{ 'Left ' + lessons.availablespace }}</span>
-          <span v-else>No lessons left</span>
+  <h1>Lesson Page</h1>
+  <section style="display: flex; flex-direction: row; width: 100%;">
+    <section style="width: 30%;">
+        <h3>Filter & Order</h3>
+        <!-- div element to encapsulate the sorting div and order div -->
+
+        <div id="filters" style="display:flex; flex-direction: column; align-self: flex-start; width: 100%;">
+            <h3 style="margin: 0.5rem 0;">Filter</h3>
+
+            <!-- div element to encapsulate the sorting input radios -->
+
+            <div id="sort">
+                <div v-for="(value, key) in sort_dict" :key="value.id">
+                  
+                    <input type="radio" name="sort" value="key" v-if="value.checked" checked v-on:click="sort(key)">
+                    <input type="radio" name="sort" value="key" v-else v-on:click="sort(key)">
+                    <label for="sort">{{key}}</label>
+                </div>
+            </div>
+            <h3 style="margin: 0.5rem 0;">Order</h3>
+
+            <!-- div element to encapsulate the order input radios -->
+
+            <div id="order">
+                <div v-for="(value, key) in order_dict" :key="value.id"> 
+                    <input type="radio" name="order" value="key" v-if="value.checked" checked v-on:click="order(key)">
+                    <input type="radio" name="order" value="key" v-else v-on:click="order(key)">
+                    <label for="order">{{key}}</label>
+                </div>
+            </div>
         </div>
+    </section>
+
+    <section style="width: 100%;">
+      <div style="width: 100%; display: inline-flex; align-items: center; justify-content: flex-start;">
+          <div id="search">
+              <input type="text" v-bind:placeholder="searchTerm" v-if="search" v-model="searchInput">
+              <button v-on:click="search"><i class='bx bx-search'></i></button>
+          </div>
+          
       </div>
-    </div>
-  </div>
+      
+      <div id="classlists">
+          <div class="classlist" v-for="item in classlist_copy" :key="item.id">
+            <img class="image" :src="item.image">
+            <div class="info_container">
+                <p>Subject: {{item.subject}}</p>
+                <p>Location: {{item.location}}</p>
+                <p>Price: ${{item.price}}</p>
+                <p>Spaces: {{item.spaces}}</p>
+                <button v-if="item.spaces > 0" @click="add_to_cart(item)">Add To Cart</button>
+                <button v-else disabled>Add To Cart</button>
+            </div>
+          </div>
+      </div>
+  </section>
+    
+</section>
 </template>
 
 <script>
+
 export default {
-  name: 'LessonItem',
-  props: ['sortedLessons'],
-  methods: {
-    canAddToCart(lessons) {
-      this.$emit('canAddToCart', lessons);
+  name: 'LessonComp',
+  props:{
+        classlistItems: {
+            type: Array,
+        }
     },
+  emits: ['add-cart'],
+  created(){
+    this.loadClasslists()
   },
-};
+  methods:{
+    
+    showCheckout(){
+        this.showClasslists = this.showClasslists ? false : true
+    },
+    add_to_cart(item){
+        const id = `${item.subject[0]}`+item.id+`${item.location[0]}`+Math.ceil(Math.random()*1000000)
+        let data = {
+            "id": id,
+            "item":item,
+        }
+        this.$emit("add-cart",JSON.stringify(data))
+    },
+    search_func(query){
+        let val = this.sort_value.toLowerCase()
+
+        if(val != "all" & query.trim() != ''){
+
+            if(val != "price"){
+                this.classlist_copy = this.classlistItems.filter(x => x[val].toLowerCase().includes(query.toLowerCase()))
+            }else{
+                if(query.match(/^[0-9]+$/) != null){
+                    this.classlist_copy = this.classlistItems.filter(x => (''+x[val]).includes(query))
+                }
+            }
+            this.order_sort(this.sort_value)
+        }
+
+        
+        if(query.trim() == ''){
+            this.loadClasslists()
+            this.order_sort(this.sort_value)
+        }
+
+    },
+    order(value){
+        this.order_value = value
+        this.order_sort(this.sort_value)
+    },
+    sort(value){
+        this.sort_value = value
+        this.order_sort(value)
+    },
+    order_sort(criteria){
+        criteria = criteria.toLowerCase()
+        if(criteria != "all"){
+            this.searchTerm = criteria
+            const isAscending = this.order_value == "Ascending"
+            this.classlist_copy.sort((a,b)=>{
+                if(a[criteria] > b[criteria]) return isAscending ? 1 : -1
+                if(b[criteria] > a[criteria]) return isAscending ? -1 : 1                       
+                return 0
+            })
+        }else{
+            this.searchTerm = "Please choose criteria eg. subject, location, price."
+            this.loadClasslists()
+        }
+    },
+    loadClasslists() {
+      this.classlist_copy = []
+    
+      this.classlistItems.forEach(x=>{
+        this.classlist_copy.push(x);
+      })
+    }
+  },
+  data(){
+    return{
+      sitename:"After School Activities Store",
+      cart:[],
+      order_value:"Ascending",
+      order_dict:{
+          "Ascending":{id:1,checked:true},
+          "Descending":{id:2,checked:false}
+      },
+      sort_value:"All",
+      sort_dict:{
+          "All":{id:1,checked:true},
+          "Subject":{id:2,checked:false},
+          "Location":{id:3,checked:false},
+          "Price":{id:4,checked:false}
+      },
+      searchInput:'',
+      searchTerm:'Please choose criteria eg. subject, location, price.',
+      
+      classlist_copy:[],
+      user_D:{
+          name:"",
+          phone:"",
+      },
+    }
+  },
+  computed:{
+    cartItemCount(){
+        return this.cart.length || "Empty";
+    },
+    removeFromcart(){
+        return this.cart.length > 0
+    },
+    search(){
+        this.search_func(this.searchInput)
+        return true
+    },
+  }
+}
 </script>
+
+<style scoped>
+ 
+</style>
