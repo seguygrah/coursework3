26fbd59c4ddb9887ff23aa0e9288c33e4d3cecf3
seguygrah <124881804+seguygrah@users.noreply.github.com>
2023-06-30 12:40:17 +0400

    available space fixed. needs tp update space & pic

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,113 @@
 <template>
-  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-<header>
-  <h1>{{sitename}}</h1>
-  <div style="align-self: flex-end; margin-right: 2rem;" v-if="showClasslists">
-      <button style="font-size: 1.15rem;" v-if="removeFromcart" @click="cycle">
-          {{cartItemCount}}
+  <div>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <header>
+      <h1>{{ sitename }}</h1>
+      <div v-if="showClasslists">
+        <button v-if="uremoveFromCart" @click="cycle">
+          {{ cartItemCount }}
           <i class="bx bx-cart"></i>
           Checkout
-      </button>
-      <button v-else style="font-size: 1.15rem;" disabled="disabled">
-          {{cartItemCount}}
+        </button>
+        <button v-else disabled="disabled">
+          {{ cartItemCount }}
           <i class="bx bx-cart"></i>
-      </button>
+        </button>
+      </div>
+      <div v-else>
+        <button @click="cycle">
+          <i class="bx bx-left-arrow-alt"></i>
+          Go back
+        </button>
+      </div>
+    </header>
+    <main>
+      <component :is="currentView" :cart-items="cart" :classlist-items="classlists" @add-cart="handleAddToCart" @remove-cart="handleRemoveFromCart" @order-submitted="handleOrderSubmitted" />
+    </main>
   </div>
-  <div v-else>
-    <button style="font-size: 1.15rem;" @click="cycle">
-        <i class='bx bx-left-arrow-alt'></i>
-        Go back
-    </button>
-  </div>
-  <!-- <button @click="cycle" >{{currentViewStr}}</button> -->
-</header>
-<main>
-  <component :is="currentView" :cart-items="cart" :classlist-items="classlists"  
-  @add-cart="handleAddToCart"  @remove-cart="handleRemoveFromCart" 
-  @order-submitted="handleOrderSubmitted"/>
-</main>
 </template>
 
 <script>
-import Checkout from './components/Cart.vue'
+import Checkout from './components/Checkout.vue'
 import Lesson from './components/Lesson.vue'
 
-
 export default {
-name: 'App',
-created(){
- this.loadClasslists()
-},
-computed:{
-  cartItemCount(){
-      return this.cart.length || "Empty";
+  name: 'App',
+  created() {
+    this.loadClasslists()
   },
-  removeFromcart(){
+  computed: {
+    cartItemCount() {
+      return this.cart.length || 'Empty'
+    },
+    uremoveFromCart() {
       return this.cart.length > 0
+    },
   },
-},
-data(){
+  data() {
     return {
-        sitename:"After School Activities Store",
-        currentView: Lesson,
-        currentViewStr: "Cart",
-        showClasslists:true,
-        cart:[],
-        classlists:[]
+      sitename: 'After School Activities Store',
+      currentView: Lesson,
+      showClasslists: true,
+      cart: [],
+      classlists: [],
     }
-},
-methods:{
-  loadClasslists(){
-    const port = 3000
-    fetch(`http://localhost:${port}/collection/lessons`).then((res)=>{
-      res.json().then((json)=>{
-          this.classlists = json.map((x)=>{
-              x["image"] = `/assets/${x["image"]}`
-              return x
+  },
+  methods: {
+    loadClasslists() {
+      const port = 3000
+      fetch(`http://localhost:${port}/collection/courses`).then((res) => {
+        res.json().then((json) => {
+          this.classlists = json.map((x) => {
+            x.image = `public/images/${x.image}`
+            return x
           })
+        })
       })
-    })
-  },
-  handleAddToCart(data) {
-    data = JSON.parse(data)
-    for (let i = 0; i < this.classlists.length; i++) {
+    },
+    handleAddToCart(data) {
+      data = JSON.parse(data)
+      const item = data.item
+      for (let i = 0; i < this.classlists.length; i++) {
         const classlist = this.classlists[i]
-        if(data["item"]._id == classlist._id){
-            classlist.spaces--
+        if (item._id === classlist._id) {
+          item.availablespace--
+          break
         }
-    }
-    this.cart.push(data)
-  },
-  handleRemoveFromCart(data) {
-    data = JSON.parse(data)
-
-    const temp = []
-    for (let i = 0; i < this.cart.length; i++) {
-        const cart_item = this.cart[i]
-        if(data["id"] != cart_item["id"]){
-          temp.push(cart_item)
-        }
-    }
-
-    this.cart = []
-
-    temp.forEach(x =>{
-      this.cart.push(x)
-    })
-    for (let i = 0; i < this.classlists.length; i++) {
+      }
+      this.cart.push(data)
+    },
+    handleRemoveFromCart(data) {
+      data = JSON.parse(data)
+      const itemId = data.id
+      const filteredCart = this.cart.filter((item) => item.id !== itemId)
+      this.cart = filteredCart
+      const item = data.item
+      for (let i = 0; i < this.classlists.length; i++) {
         const classlist = this.classlists[i]
-        if(data["item"]._id == classlist._id){
-            classlist.spaces++
+        if (item._id === classlist._id) {
+          item.availablespace++
+          break
         }
-    }
-  },
-  handleOrderSubmitted(){
-    this.cart = []
-    this.cycle()
-  },
-  cycle(){
-      if(this.currentView["name"] == Lesson["name"]){
+      }
+    },
+    handleOrderSubmitted() {
+      this.cart = []
+      this.cycle()
+    },
+    cycle() {
+      if (this.currentView.name === Lesson.name) {
         this.currentView = Checkout
-        this.currentViewStr = "Go Back"
         this.showClasslists = false
-      }else{
+      } else {
         this.currentView = Lesson
-        this.currentViewStr = "Cart"
         this.showClasslists = true
       }
-  }
-}
+    },
+  },
 }
 </script>
 
 <style>
-
+/* ... Rest of the styles ... */
 </style>
